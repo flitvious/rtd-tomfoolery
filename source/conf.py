c0c2e29cb40c0d8dbb26cdd8809dc205bcd4d27d
Pavel Salnikov <flitvious@users.noreply.github.com>
2018-08-31 13:39:41 +0300

    bring back rtd

--- source/conf.py
+++ source/conf.py
@@ -76,7 +76,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -167,8 +167,8 @@ todo_include_todos = True
 
 # -- Prolog substitutions (available on all pages)
 
-#tomfool_names = 'Jon, Snide, and Hector'
-#project_name = 'Tomfoolery guide'
+tomfool_names = 'Jon, Snide, and Hector'
+project_name = 'Tomfoolery guide'
 
 rst_prolog = """
 .. |tomfool_names| replace:: {0}
